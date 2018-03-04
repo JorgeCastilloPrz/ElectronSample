419ba75f5e394267b5815b583837ec5b4d046d53
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-04 19:51:18 +0100

    render google map with marker

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,17 +1,25 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import logo from "../assets/logo.svg";
 import "../css/App.css";
+import MapView from "./MapView.jsx"
 
 export default class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">Hello Buzz!</p>
+    return (<div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h2>Random Map React component using <a href="https://github.com/istarkov/google-map-react">google-map-react</a> from
+          <p>
+            <a href="https://github.com/istarkov">@istarkov</a>
+          </p>
+        </h2>
       </div>
-    );
+      <div style={{
+          width: '100%',
+          height: '70vh'
+        }}>
+        <MapView/>
+      </div>
+    </div>);
   }
 }
