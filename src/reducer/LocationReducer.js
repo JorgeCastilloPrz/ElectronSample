58680158026a4688b256b3943c94bc8bca0686e5
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-06 20:33:22 +0100

    add new action to add a current location

--- src/reducer/LocationReducer.js
+++ src/reducer/LocationReducer.js
@@ -1,13 +1,37 @@
-import ADD_LOCATION from '../actions/actions.js' 
+import {
+  ADD_LOCATION,
+  ADD_CURRENT_LOCATION
+} from '../actions/actions.js'
+import addLocation from '../actions/actions.js'
 
 const initialState = {
+  currentLocation: {
+    latitude = 0,
+    longitude = 0
+  },
   locations: []
 }
 
 function nearbyLocationsAppReducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_CURRENT_LOCATION:
+      return Object.assign({}, state, {
+        currentLocation: {
+          latitude = action.latitude,
+          longitude = action.longitude
+        }
+      })
     case ADD_LOCATION:
-      return { ...state, visibilityFilter: action.filter }
+      return Object.assign({}, state, {
+        todos: [
+          ...state.locations,
+          {
+            latitude: action.latitude,
+            longitude: action.longitude
+          }
+          addLocation(action.index, action.latitude, action.longitude)
+        ]
+      })
     default:
       return state
   }
