ae8340e435312111646e757597e515f6f53b5e06
Jorge Castillo <jorge.castillo.prz@gmail.com>
2018-03-06 20:33:22 +0100

    add new action to add a current location

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,15 @@
 export const ADD_LOCATION = 'ADD_LOCATION'
+export const ADD_CURRENT_LOCATION = "ADD_CURRENT_LOCATION"
+
+// Actions are created by Action Creators which are defined with simpe functions
+// in Redux.
 
 /**
- * Actions are created by Action Creators which are defined with simpe functions
- * in Redux.
  * @param {int} index - The index for the Action.
  * @param {float} latitude - The location latitude.
  * @param {float} longitude - The location longitude.
+ *
+ * @return an ADD_LOCATION action with the given parameters.
  */
 function addLocation(index, latitude, longitude) {
   return {
@@ -15,6 +19,20 @@ function addLocation(index, latitude, longitude) {
     longitude: longitude
   }
 }
+
+/**
+ * @param {float} latitude - The current location latitude.
+ * @param {float} longitude - The current location longitude.
+ *
+ * @return an ADD_CURRENT_LOCATION action with the given parameters.
+ */
+ function addCurrentLocation(latitude, longitude) {
+   return {
+     type: ADD_CURRENT_LOCATION,
+     latitude: latitude,
+     longitude: longitude
+   }
+ }
 
 /**
  * Arrow function (lambda) to return a list of actions.
